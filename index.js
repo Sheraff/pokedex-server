fb7e97d4cc1f5a4ab0415415169fe0be9c3755cc
Flo <florian.pellet@matera.eu>
2022-12-26 18:24:18 +0100

    fix jsons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const { readFileSync } = require('node:fs')
+const { writeFile } = require('node:fs/promises')
 const { join } = require('node:path')
 
 const port = 8080
@@ -10,38 +11,47 @@ const app = express()
 const str = readFileSync(join(__dirname, 'all.json'), 'utf8')
 const data = JSON.parse(str)
 
-app.get('/api', (req, res) => {
-	res.send(`
-		<h1>Pokedex API</h1>
-		<table>
-			<tr>
-				<td><a href="/api/list">List of pokemon</a></td>
-				<td><code>/api/list</code></td>
-			</tr>
-			<tr>
-				<td><a href="/api/pokemon/1">Pokemon by id</a></td>
-				<td><code>/api/pokemon/:id</code></td>
-			</tr>
-			<tr>
-				<td><a href="/pokedex-server/images/1.png">Pokemon image by id</a></td>
-				<td><code>/pokedex-server/images/:id.png</code></td>
-			</tr>
-		</table>
-	`)
-})
+void async function() {
+	// data.forEach(item => item.ThumbnailImage = `/pokedex-server/images/${item.id}.png`)
+	// writeFile(join(__dirname, 'all.json'), JSON.stringify(data, null, 2), 'utf8')
+	for (const item of data) {
+		await writeFile(join(__dirname, 'pokemon', `${item.id}.json`), JSON.stringify(item, null, 2), 'utf8')
+	}
+}()
 
-app.get('/api/list', (req, res) => {
-	res.send(data.map((item) => item.id))
-})
 
-app.get('/api/pokemon/:id', (req, res) => {
-	const { id } = req.params
-	const pokemon = data.find((item) => item.id === Number(id))
-	res.send(pokemon)
-})
+// app.get('/api', (req, res) => {
+// 	res.send(`
+// 		<h1>Pokedex API</h1>
+// 		<table>
+// 			<tr>
+// 				<td><a href="/api/list">List of pokemon</a></td>
+// 				<td><code>/api/list</code></td>
+// 			</tr>
+// 			<tr>
+// 				<td><a href="/api/pokemon/1">Pokemon by id</a></td>
+// 				<td><code>/api/pokemon/:id</code></td>
+// 			</tr>
+// 			<tr>
+// 				<td><a href="/pokedex-server/images/1.png">Pokemon image by id</a></td>
+// 				<td><code>/pokedex-server/images/:id.png</code></td>
+// 			</tr>
+// 		</table>
+// 	`)
+// })
 
-app.use('/api/images', express.static(join(__dirname, 'images')))
+// app.get('/api/list', (req, res) => {
+// 	res.send(data.map((item) => item.id))
+// })
 
-app.listen(port, () => {
-	console.log(`Pokedex api listening on port ${port}`)
-})
+// app.get('/api/pokemon/:id', (req, res) => {
+// 	const { id } = req.params
+// 	const pokemon = data.find((item) => item.id === Number(id))
+// 	res.send(pokemon)
+// })
+
+// app.use('/api/images', express.static(join(__dirname, 'images')))
+
+// app.listen(port, () => {
+// 	console.log(`Pokedex api listening on port ${port}`)
+// })
